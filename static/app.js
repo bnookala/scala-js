4f1258cd769eac6b2246d9b57c3aa3de96988ff8
Bhargav Nookala <nooknb@gmail.com>
2013-01-25 09:09:09 -0800

    Adding title, fixing JS

--- static/app.js
+++ static/app.js
@@ -1,17 +1,28 @@
 $(window).load(function () {
-    var map = L.map('map').setView([51.505, -0.09], 13);
-    var apiKey = "f39f2318cf924151bab139be0e677f8e";
-    var tileURL = ['http://{s}.tile.cloudmade.com/', apiKey, '/997/256/{z}/{x}/{y}.png'].join('');
-    L.tileLayer(tileURL, {maxZoom: 18}).addTo(map);
-    var marker = L.marker([51.5, -0.09]).addTo(map);
-    var circle = L.circle([51.508, -0.11], 500, {
-            color: 'red',
-            fillColor: '#f03',
-            fillOpacity: 0.5
-    }).addTo(map);
-    var polygon = L.polygon([
-            [51.509, -0.08],
-            [51.503, -0.06],
-            [51.51, -0.047]
-    ]).addTo(map);
+    var map = new SJSMap();
+    map.addAllMarkers();
 });
+
+var SJSMap = function () {
+    this.map = L.map('map').setView([37.8004, -122.345], 13);
+    this.apiKey = "f39f2318cf924151bab139be0e677f8e";
+    var tileURL = ['http://{s}.tile.cloudmade.com/', this.apiKey, '/997/256/{z}/{x}/{y}.png'].join('');
+    L.tileLayer(tileURL, {maxZoom: 18}).addTo(this.map);
+};
+
+SJSMap.prototype.data = [];
+
+SJSMap.prototype.addAllMarkers = function () {
+    $.get('/static/openpaths.json', $.proxy(this.onAllMarkersReceived, this));
+};
+
+SJSMap.prototype.onAllMarkersReceived = function (data) {
+    var context = this;
+    $.each(data, function (idx, elem) {
+        context.addMarker(elem['lat'], elem['lon']);
+    });
+};
+
+SJSMap.prototype.addMarker = function (lat, lon) {
+    var marker = L.marker([lat, lon]).addTo(this.map);
+};
